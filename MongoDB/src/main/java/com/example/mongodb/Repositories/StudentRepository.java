5d1c8a67bdbac95ea3cc7c4dd27f01d29cd11f5b
Bui Hoang Duy <hoangduygala@gmail.com>
2024-11-21 23:29:10 +0700

    idk

--- MongoDB/src/main/java/com/example/mongodb/Repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mongodb.Repositories;
-
-import com.example.mongodb.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentRepository extends MongoRepository<Student, Integer> {
-
-    }
