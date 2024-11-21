5ad896627686977ada0eaf6a7a13571a6e98f32a
huytan <nghuytan.ptit@gmail.com>
2024-11-21 16:06:06 +0700

    db

--- carmanager.sql
+++ carmanager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 21, 2024 lúc 10:02 AM
+-- Thời gian đã tạo: Th10 21, 2024 lúc 10:05 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.0.30
 
@@ -20,6 +20,64 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `carmanager`
 --
+CREATE DATABASE IF NOT EXISTS `carmanager` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `carmanager`;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Cắt ngắn bảng trước khi chèn `customer`
+--
+
+TRUNCATE TABLE `customer`;
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `salary` varchar(255) DEFAULT NULL,
+  `bonus_salary` varchar(255) DEFAULT NULL,
+  `default_salary` varchar(255) DEFAULT NULL,
+  `cars_sold_total` int(11) DEFAULT NULL,
+  `manager_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Cắt ngắn bảng trước khi chèn `employee`
+--
+
+TRUNCATE TABLE `employee`;
+--
+-- Đang đổ dữ liệu cho bảng `employee`
+--
+
+INSERT INTO `employee` (`id`, `username`, `password`, `role`, `name`, `phone_number`, `email`, `address`, `salary`, `bonus_salary`, `default_salary`, `cars_sold_total`, `manager_id`) VALUES
+(1, 'employee1', '$2a$10$O1A5L.nz6X.VHtO8ye9aKuzmdrGBn80W3okE.rWxTwArAqJZBrMjq', 'ROLE_EMPLOYEE', 'Jane Smith', '987654321', 'employee1@example.com', '456 Elm St', '3000', '500', '2500', 0, NULL),
+(2, 'employee2', '$2a$10$FK1dRtxrEGWdiT9.MKfXYuqDHrLb8aw2YtiFhSocBocjfFeRMeaBK', 'ROLE_EMPLOYEE', 'Jane Smith', '987654321', 'employee1@example.com', '456 Elm St', '3000', '500', '2500', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -59,6 +117,20 @@ INSERT INTO `manager` (`id`, `username`, `password`, `role`, `name`, `phone_numb
 --
 
 --
+-- Chỉ mục cho bảng `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `fk_manager_id` (`manager_id`);
+
+--
 -- Chỉ mục cho bảng `manager`
 --
 ALTER TABLE `manager`
@@ -70,10 +142,33 @@ ALTER TABLE `manager`
 --
 
 --
+-- AUTO_INCREMENT cho bảng `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT cho bảng `manager`
 --
 ALTER TABLE `manager`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_manager_id` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
